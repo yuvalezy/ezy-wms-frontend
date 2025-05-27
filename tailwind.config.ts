53102df8ab473726ac71a58a4cec6dbdb9151593
Yuval Lerner <yuval.lerner@beonesolutions.com>
2025-05-27 16:34:25 -0500

    Save changes

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import { type Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    function({ addVariant }: any) {
+      addVariant('hocus', '&:hover, &:focus');
+    },
+  ],
+}
+export default config
