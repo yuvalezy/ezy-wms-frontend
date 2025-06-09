3bb7ac47d6013b071c41e381305197acb6b0a214
Yuval Lerner <yuval@ezyprop.com>
2025-06-09 09:39:09 -0500

    Done with warehouse name

--- src/Components/AppSidebar.tsx
+++ src/Components/AppSidebar.tsx
@@ -137,6 +137,12 @@ export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarContent>
+
+        <div className="px-4 py-3 border-b border-gray-200">
+          <div className="text-sm font-medium text-gray-900">
+            {user?.warehouses?.find(v => v.id === user?.currentWarehouse)?.name}
+          </div>
+        </div>
         <SidebarGroup>
           <SidebarMenu>
             <SidebarMenuItem>
